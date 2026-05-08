897d0e9f7a0f8f8d2104ec820a37c0cdf8c664b8
vboxuser <vboxuser@debby12.myguest.virtualbox.org>
2026-05-08 10:15:20 +0100

    feat(install.sh):Conversion of Makefile to install.sh

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+line='-----'
+
+echo $line"Git add"
+git add .
+
+echo $line"Git commit"
+git commit -m "feat(install.sh):Conversion of Makefile to install.sh"
+
+echo $line"Git push"
+git push
