461ae186584103df2b7df02ab92592f938dd3679
Sprocket T Goat <sprocket@esharpishly.com>
2026-05-17 15:12:23 +0100

    feat(Email):Added LeadsController base code

---
+++ web/php/src/Controllers/LeadsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\LeadsModel;
+
+class LeadsController extends BaseController {
+    /**
+     * Display all leads
+     */
+    public function index(){
+
+    }
+}
