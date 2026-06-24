6a59cd74e455fc43e8c58836385f9ecb99773280
Sprocket T Goat <sprocket@esharpishly.com>
2026-06-24 15:48:46 +0100

    feat(form ingestion):scaffold form form ingestion

--- web/php/src/Controllers/IngestionController.php
+++ web/php/src/Controllers/IngestionController.php
@@ -6,13 +6,53 @@ use App\Models\IngestionModel;
 
 class IngestionController extends BaseController {
 
+    /**
+     * Get form field names so they can be mapped to $data array
+     */
+    public function index(){
+
+        // 1.  URL will be retrieved from db
+        $url = 'https://www.applybe.com/?a=145F80311.0';
+
+        // 2. get form contents
+        $contents = file_get_contents($url);
+
+        // 3. add contents to storage
+        // file_put_contents()
+
+        // 4. save form field names to db
+
+        // 5. respond when completed
+
+    }
+
+    public function setFields($data){
+
+        // ask rag to find form fields and correctly map them to data array
+        $conditions = array(
+            'tbl' => 'forms',
+            'where' => array('id' => 1)
+        );
+
+        $res = $this->db->find($conditions);
+
+        // map form field names to data
+        //  map($data,$res);
+
+
+        return $data;
+    }
+
     public function save() {
         //http://192.168.0.218/php/ingestion/save/?url=https://www.applybe.com/?a=145F80311.0
         $url = $_GET['url'] ?? '';
+        
         $data = [
             'FirstName' => 'Paul', 
             'email' => 'paul@sharpishly.com'
         ];
+
+        $data = 
 
         $parser = new IngestionModel();
         $html = $parser->fetchRaw($url);
